9892d3cebb7a36ab43b318647858b4ac24db54bc
LamDoon <71416214+LamDoon@users.noreply.github.com>
2021-06-20 10:12:42 +0700

    Add files via upload

---
+++ NgoLamDoon_CuoiKi/ModelEF/DAO/adminDAO.cs
@@ -0,0 +1,35 @@
+﻿using ModelEF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEF.DAO
+{
+  public  class adminDAO
+    {
+        private NgoLamDoonContext db;
+
+        public adminDAO()
+        {
+            db = new NgoLamDoonContext();
+        }
+
+        public List<UserAccount> ListAll()
+        {
+            return db.UserAccounts.ToList();
+        }
+
+        public int login(string UserName, string Password)
+        {
+            var result = db.UserAccounts.SingleOrDefault(x => x.UserName.Contains(UserName) && x.Password.Contains(Password) && x.Status.Contains("Active") );
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+                return 1;
+        }
+    }
+}
